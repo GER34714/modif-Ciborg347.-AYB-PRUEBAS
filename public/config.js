e3d60592b1799016f1b07406ae6e79ae7626d5b8
ciborg 347 <225079095+GER34714@users.noreply.github.com>
2026-06-04 15:01:15 -0300

    Create config.js

---
+++ public/config.js
@@ -0,0 +1,6 @@
+// config.js - Configuración centralizada
+window.CIBORG_CONFIG = {
+  WHATSAPP_NUMBER: "5491164499481",
+  SUPABASE_URL: "https://fhxcumwhgtfirznnznjx.supabase.co",
+  SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoeGN1bXdoZ3RmaXJ6bm56bmp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5OTYyNzEsImV4cCI6MjA4MzU3MjI3MX0.7z1B099L4yrA9k1JxwvYGCABzqiqYtkUClI3E8wQ2zA"
+};
